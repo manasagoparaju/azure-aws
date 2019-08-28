daab28f826f3dec229d514653eaafb75a780df7a
manasagoparaju <53125170+manasagoparaju@users.noreply.github.com>
2019-08-28 22:56:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const app = express();
+
+
+//DB config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true})
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+
+//Ejs
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Bodyparser
+app.use(express.urlencoded({ extended: false}) );
+
+// Routess
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log('Server started on port ${PORT}'));
